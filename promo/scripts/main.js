4545996b6922b1736fd022a1b4a9dbe731185c05
Pavel Laptev <pavellaptev@Pavels-MacBook-Pro.local>
2019-01-07 03:11:56 +0500

    styles fix

--- promo/scripts/main.js
+++ promo/scripts/main.js
@@ -1,1 +1,34 @@
-console.log("yep");
+const macbook = document.getElementById("macbook-device");
+const iphone = document.getElementById("iphone-device");
+const ipad = document.getElementById("ipad-device");
+
+const macbookPos = ["-6px -1781px", "-6px -2373px", "-917px -2372px"];
+const iphonePos = ["-5px -1733px", "207.5px -1733px", "421.5px -1733px"];
+const ipadPos = ["-3px -2134px", "496px -2134px", "997px -2134px"];
+
+///////
+macbook.addEventListener("mouseenter", function() {
+    this.style.backgroundPosition = macbookPos[Math.floor(Math.random() * 3)];
+});
+
+macbook.addEventListener("mouseleave", function() {
+    this.style.backgroundPosition = macbookPos[Math.floor(Math.random() * 3)];
+});
+
+///////
+iphone.addEventListener("mouseenter", function() {
+    this.style.backgroundPosition = iphonePos[Math.floor(Math.random() * 3)];
+});
+
+iphone.addEventListener("mouseleave", function() {
+    this.style.backgroundPosition = iphonePos[Math.floor(Math.random() * 3)];
+});
+
+///////
+ipad.addEventListener("mouseenter", function() {
+    this.style.backgroundPosition = ipadPos[Math.floor(Math.random() * 3)];
+});
+
+ipad.addEventListener("mouseleave", function() {
+    this.style.backgroundPosition = ipadPos[Math.floor(Math.random() * 3)];
+});
