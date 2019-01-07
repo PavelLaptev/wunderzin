94c1b992cf6a39ce7766c839d2c87052748efd15
Pavel Laptev <pavellaptev@Pavels-MacBook-Pro.local>
2019-01-07 12:23:11 +0500

    script update

--- promo/scripts/main.js
+++ promo/scripts/main.js
@@ -11,24 +11,12 @@ macbook.addEventListener("mouseenter", function() {
     this.style.backgroundPosition = macbookPos[Math.floor(Math.random() * 3)];
 });
 
-macbook.addEventListener("mouseleave", function() {
-    this.style.backgroundPosition = macbookPos[Math.floor(Math.random() * 3)];
-});
-
 ///////
 iphone.addEventListener("mouseenter", function() {
     this.style.backgroundPosition = iphonePos[Math.floor(Math.random() * 3)];
 });
 
-iphone.addEventListener("mouseleave", function() {
-    this.style.backgroundPosition = iphonePos[Math.floor(Math.random() * 3)];
-});
-
 ///////
 ipad.addEventListener("mouseenter", function() {
-    this.style.backgroundPosition = ipadPos[Math.floor(Math.random() * 3)];
-});
-
-ipad.addEventListener("mouseleave", function() {
     this.style.backgroundPosition = ipadPos[Math.floor(Math.random() * 3)];
 });
